b74747abe70c547e87d290967851f4d86c500d82
Kaja <ayreeve@wp.pl>
2017-04-29 13:18:50 +0200

    triangleArea var added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 /*jslint devel: true*/
 
-var name = prompt('What is your name?');
-//alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var name = prompt('Jak masz na imię?');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+var a = prompt('Podaj długość podstawy trójkąta (w cm)');
+var h = prompt('Podaj wysokość trójkąta (w cm)');
+var triangleArea = a * h / 2;
+alert('Pole trójkąta wynosi ' + triangleArea + ' cm.');
+console.log('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi ' + triangleArea);
