1788288ba89037c8cf00400e43a71b982952cdb6
Kaja <ayreeve@wp.pl>
2017-04-29 18:05:37 +0200

    conditional statement for negative/postive numbers added

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,9 @@ var h = prompt('Podaj wysokość trójkąta (w cm)');
 var triangleArea = a * h / 2;
 alert('Pole trójkąta wynosi ' + triangleArea + ' cm.');
 console.log('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi ' + triangleArea);
+
+var x = 1,
+    y = 2,
+    value = (x * x) + (2 * x * y) - (y * y);
+
+var value = value >= 0 ? console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą dodatnią.') : console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą ujemną.');
