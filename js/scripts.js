763574bb09b12f3862717d7172a70810e8d5f4c5
Kaja <ayreeve@wp.pl>
2017-05-04 13:29:31 +0200

    TriagnleArea function added

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,17 @@ var x = 1,
     value = (x * x) + (2 * x * y) - (y * y);
 
 var value = value >= 0 ? console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą dodatnią.') : console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą ujemną.');
+
+function getTriangleArea(a, h) {
+    if (a > 0 && h > 0) {
+        return a * h / 2;
+    } else {
+        return 'Nieprawidłowe dane';
+    }
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = console.log(getTriangleArea(5, 2));
+var triangle2Area = console.log(getTriangleArea(99, 789));
+var traingle3Area = console.log(getTriangleArea(12, 71));
