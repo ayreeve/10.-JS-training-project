9ad2bb4258ea8fd5e0e65fec7268997960c9aa44
Kaja <ayreeve@wp.pl>
2017-05-05 10:16:53 +0200

    array and comments added, structure changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 /*jslint devel: true*/
 
+/* welcome */
 var name = prompt('Jak masz na imię?');
 alert('Witaj, ' + name);
+
 console.log('Witaj, ' + name);
 
-var a = prompt('Podaj długość podstawy trójkąta (w cm)');
-var h = prompt('Podaj wysokość trójkąta (w cm)');
-var triangleArea = a * h / 2;
+/* traingle area */
+var a = prompt('Podaj długość podstawy trójkąta (w cm)'),
+    h = prompt('Podaj wysokość trójkąta (w cm)'),
+    triangleArea = a * h / 2;
 alert('Pole trójkąta wynosi ' + triangleArea + ' cm.');
+
 console.log('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi ' + triangleArea);
 
+/* operation */
 var x = 1,
     y = 2,
-    value = (x * x) + (2 * x * y) - (y * y);
+    value = (x * x) + (2 * x * y) - (y * y),
+    value = value >= 0 ? console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą dodatnią.') : console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą ujemną.');
 
-var value = value >= 0 ? console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą dodatnią.') : console.log('Wynik działania (x * x) + (2 * x * y) - (y * y) (równy ' + value + ') jest liczbą ujemną.');
-
+/* triangle area conditional statement */
 function getTriangleArea(a, h) {
     if (a > 0 && h > 0) {
         return a * h / 2;
@@ -26,6 +31,20 @@ function getTriangleArea(a, h) {
 
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area = console.log(getTriangleArea(5, 2));
-var triangle2Area = console.log(getTriangleArea(99, 789));
-var traingle3Area = console.log(getTriangleArea(12, 71));
+var triangle1Area = console.log(getTriangleArea(5, 2)),
+    triangle2Area = console.log(getTriangleArea(99, 789)),
+    traingle3Area = console.log(getTriangleArea(12, 71));
+
+/* array */
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+    allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+}
+
+console.log(allNames);
